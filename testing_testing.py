02df24e2e5402d8d4b1ff4e636c94e2fab6e7300
ilyaaltshteyn <ilyaaltshteyn@gmail.com>
2020-12-22 16:39:52 -0800

    changed line 2

--- testing_testing.py
+++ testing_testing.py
@@ -1,3 +1,3 @@
 print("testing testing")
-print("this is a line i added while on my new fixing_bug1 branch")
+print("line i added while on my new fixing_bug1 branch")
 print('Matt was here and made a very important change")
